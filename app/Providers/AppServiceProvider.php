07c844700f7f04bb5fb731698ec586be5c090f88
ismoil <ismoil3436@gmail.com>
2025-01-11 15:01:03 +0500

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider;use Illuminate\Support\Facades\View;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        View::composer('*', function ($view) {
+            $cart = session()->get('cart', []);
+            $totalItems = array_sum(array_column($cart, 'quantity'));
+            $view->with('cartCount', $totalItems);
+        });
     }
 }
