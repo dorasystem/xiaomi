6474fe916e0a77456e1021e6d087b55aed0aee9b
muyassard <muyassarsolijonova@gmail.com>
2025-01-11 14:44:39 +0500

    *

--- resources/views/pages/single-news.blade.php
+++ resources/views/pages/single-news.blade.php
@@ -11,24 +11,26 @@ $lang = \Illuminate\Support\Facades\App::getLocale();
             </div>
             <hr />
         </div>
-        <div class="row align-items-center justify-content-between mb-lg-0 mb-2">
+        {{-- <div class="row align-items-center justify-content-between mb-lg-0 mb-2">
             <h2 class="order-lg-1 order-2 col-lg-10 fw-normal hover-orange mb-3">{{ $news['title_' . $locale] ?? ' ' }}</h2>
             <div class="order-lg-2 order-1 col-lg-2 d-flex align-items-center gap-2">
                 <img src="/assets/icons/calendar-icon.svg" alt="" />
                 <div class="text-grey">{{ \Carbon\Carbon::parse($news['date'])->format('d.m.Y') }}</div>
             </div>
+        </div> --}}
+         <div class="col-12 pe-lg-5 pe-2 mb-4 d-sm-block d-none">
+            <div class="newbanner w-100 rounded text-white d-flex flex-column justify-content-end"
+                style="background-image: url('{{ asset('storage/' . $news->image) }}'); height:400px;">
+                <h1 class="mb-4">
+                    {{ $news['title_' . $locale] ?? ' ' }}
+                </h1>
+                <small class="fw-bold border-top pt-3">
+                    {{ \Carbon\Carbon::parse($news['date'])->format('d.m.Y') }}</small>
+            </div>
         </div>
         <div class="mb-5">
             {!! $news['description_' . $locale] ?? 'Китайская компания готовит к выпуску свои новейшие флагманы –Xiaomi 15 и 15 Pro. Анонс состоится на специальном мероприятии: дата выхода Xiaomi 15 ожидается' !!}
             {!! $news['content_' . $locale] !!}
-{{--            <h4 class="fw-bold">Какие будут характеристики Xiaomi 15</h4>--}}
-{{--            Каждое из устройств будет оснащено впечатляющими дисплеями: стандартная модель получит FHD-экран 6.36 дюймов. А Pro-версия – FHD-дисплей 6.73 дюймов, у которой частота обновления--}}
-{{--            достигает 120 Гц. Оба телефона предложат различные конфигурации памяти, включая варианты до 16 ГБ / 1 ТБ или до 12 / 256 ГБ. Кроме того, они будут защищены от воды и пыли согласно--}}
-{{--            стандарту IP68. <br /><br />--}}
-{{--            Стандартная модель не отстает в технологическом прогрессе, будучи оснащенной аккумулятором примерно на 4900 мАч с возможностями быстрой проводной зарядки на 100 Вт и беспроводной на 50--}}
-{{--            Вт. <br /><br />--}}
-{{--            В сравнении с предшественниками версия Pro демонстрирует улучшения в системе питания и зарядных возможностях. Этот флагман оснащен аккумулятором на 5400 мАч. Предлагает опции зарядки--}}
-{{--            через кабель на 120 Вт, беспроводную на 80 Вт. В качестве новшества в устройстве появится ультразвуковой метод идентификации по отпечаткам пальцев, внедренный в экран.--}}
         </div>
         <h1 class="fw-normal hover-orange mb-4 mt-5 text-history">@lang('home.other_news')</h1>
         <div style="overflow: hidden" class="news container py-3 position-relative">
@@ -38,7 +40,7 @@ $lang = \Illuminate\Support\Facades\App::getLocale();
                         <div class="swiper-slide product shadow-sm position-relative rounded">
                             <a href="{{ route('single.news', $item->slug) }}"
                                class="mb-3">
-                                <img height="250px" class="w-100 fit-cover"
+                                <img height="250px" class="w-100 fit-cover rounded-top"
                                      src="{{ asset('storage/' . $item->image) ?? '/assets/images/news1.jpg' }}"
                                      alt="" />
                                 <div class="p-4 bg-darkgrey">
