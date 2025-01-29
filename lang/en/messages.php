f6937fe4f63aa08a8ace3d5e213a080a7f973d59
Nazarbek777 <nazarbekrashidov2@gmail.com>
2025-01-29 21:00:08 +0500

    fix

---
+++ lang/en/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'delivery' => 'Delivery',
+    'payment' => 'Payment',
+    'authenticity_check' => 'Authenticity Check',
+    'warranty' => 'Warranty',
+    'return' => 'Return',
+    'service_centers' => 'Service Centers',
+    'instructions' => 'Instructions',
+    'faq' => 'FAQ',
+    'write_text' => 'Write text',
+    'send' => 'Send',
+    'information' => 'Information',
+    'smart_home' => 'Smart Home',
+];
---
+++ lang/ru/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'delivery' => 'Доставка',
+    'payment' => 'Оплата',
+    'authenticity_check' => 'Проверка подлинности',
+    'warranty' => 'Гарантия',
+    'return' => 'Возврат',
+    'service_centers' => 'Сервисные центры',
+    'instructions' => 'Инструкции',
+    'faq' => 'Вопрос-ответ',
+    'write_text' => 'Напишите текст',
+    'send' => 'Отправить',
+    'information' => 'Информация',
+    'smart_home' => 'Умный дом',
+];
