ee1e9a495ea96fda105913e4b24db5791dc92950
Nazarbek777 <nazarbekrashidov9373@gmail.com>
2025-02-02 01:41:28 +0500

    fix

--- resources/views/pages/site/faq.blade.php
+++ resources/views/pages/site/faq.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.page')
 
 @section('content')
+    <main class="">
     <div class="container my-4 mb-5">
 
             <div class="d-flex align-items-center gap-1">
@@ -9,26 +10,31 @@
             </div>
             <hr />
 
-        <h2 class="fw-bold">@lang('home.faq')</h2>
 
-        <div class="accordion mt-4 p-4 rounded bg-white" id="faqAccordion">
-            @foreach($faqs as $index => $faq)
-                <div class="accordion-item border mt-2 rounded">
-                    <h2 class="accordion-header" id="heading{{ $index }}">
-                        <button class="accordion-button {{ $index > 0 ? 'collapsed' : '' }}" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#collapse{{ $index }}" aria-expanded="{{ $index === 0 ? 'true' : 'false' }}"
-                                aria-controls="collapse{{ $index }}">
-                            {{ $faq['question'] }}
-                        </button>
-                    </h2>
-                    <div id="collapse{{ $index }}" class="accordion-collapse collapse {{ $index === 0 ? 'show' : '' }}"
-                         aria-labelledby="heading{{ $index }}" data-bs-parent="#faqAccordion">
-                        <div class="accordion-body">
-                            {{ $faq['answer'] }}
+    </div>
+
+        <div class="container my-5">
+            <h2 class="fw-bold">@lang('home.faq')</h2>
+
+            <div class="accordion mt-4 p-4 rounded bg-white" id="faqAccordion">
+                @foreach($faqs as $index => $faq)
+                    <div class="accordion-item border mt-2 rounded">
+                        <h2 class="accordion-header" id="heading{{ $index }}">
+                            <button class="accordion-button {{ $index > 0 ? 'collapsed' : '' }}" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapse{{ $index }}" aria-expanded="{{ $index === 0 ? 'true' : 'false' }}"
+                                    aria-controls="collapse{{ $index }}">
+                                {{ $faq['question'] }}
+                            </button>
+                        </h2>
+                        <div id="collapse{{ $index }}" class="accordion-collapse collapse {{ $index === 0 ? 'show' : '' }}"
+                             aria-labelledby="heading{{ $index }}" data-bs-parent="#faqAccordion">
+                            <div class="accordion-body">
+                                {{ $faq['answer'] }}
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
-    </div>
+    </main>
 @endsection
