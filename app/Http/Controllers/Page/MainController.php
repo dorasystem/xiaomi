0cf56b41ec034a6cfbc9b71a849f238a99e5fb60
Nazarbek777 <nazarbekrashidov9373@gmail.com>
2025-03-12 13:26:20 +0500

    fix

--- app/Http/Controllers/Page/MainController.php
+++ app/Http/Controllers/Page/MainController.php
@@ -162,7 +162,6 @@ class MainController extends Controller
         $products = Product::where("name_$lang", 'like', "%$search%")
             ->with('variants') // Eager load variants
             ->get();
-        dd($products);
 
         return view('pages.search-products', compact('products', 'lang', 'search'));
     }
