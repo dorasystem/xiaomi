21abc0a53104b1fda0c3d0107a1e3c66873c73d5
Nazarbek777 <nazarbekrashidov9373@gmail.com>
2025-03-18 09:42:42 +0500

    fix

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -79,6 +79,22 @@
 
         gtag('config', 'G-BKW9WTZ3CV');
     </script>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(100319663, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/100319663" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
     <noscript><img height="1" width="1" style="display:none"
             src="https://www.facebook.com/tr?id=1056501186100448&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
